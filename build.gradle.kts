4a72fa18f56911556ccc1c79d3082e4c2b831334
limbang <495071565@qq.com>
2022-12-04 00:53:56 +0800

    :tada: 初始化项目

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    val kotlinVersion = "1.7.20"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.serialization") version kotlinVersion
+
+    id("net.mamoe.mirai-console") version "2.12.3"
+}
+
+group = "top.limbang"
+version = "0.1.0"
+
+
+repositories {
+    maven("https://maven.aliyun.com/repository/public")
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+    testImplementation(kotlin("test"))
+}
