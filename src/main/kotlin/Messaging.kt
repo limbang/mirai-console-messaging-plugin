e6e1479ee4951f66832162fa6f88048548bf2697
limbang <495071565@qq.com>
2022-12-04 18:38:04 +0800

    feat: 重构开始监听并添加全部监听的命令

--- src/main/kotlin/Messaging.kt
+++ src/main/kotlin/Messaging.kt
@@ -14,6 +14,7 @@ import kotlinx.serialization.json.Json
 import net.mamoe.mirai.console.command.CommandManager.INSTANCE.register
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
+import net.mamoe.mirai.contact.Group
 import net.mamoe.mirai.contact.nameCardOrNick
 import net.mamoe.mirai.event.GlobalEventChannel
 import net.mamoe.mirai.event.subscribeGroupMessages
@@ -23,11 +24,13 @@ import okhttp3.WebSocket
 import okhttp3.WebSocketListener
 import top.limbang.MessagingData.messagingServerList
 import top.limbang.entity.ConnectStatus
+import top.limbang.entity.MessagingServer
 import top.limbang.entity.UserMessage
 import top.limbang.utlis.WebSocketUtils
 import top.limbang.utlis.toMiraiMessage
 import java.io.EOFException
 import java.net.ConnectException
+import java.net.SocketTimeoutException
 
 
 object Messaging : KotlinPlugin(
@@ -44,13 +47,12 @@ object Messaging : KotlinPlugin(
         MessagingData.reload()
         MessagingCompositeCommand.register()
 
-        // 获取插件作用域
-        val scope = this
         // 创建事件通道
-        val eventChannel = GlobalEventChannel.parentScope(scope)
+        val eventChannel = GlobalEventChannel.parentScope(this)
 
         // 监听群消息
         eventChannel.subscribeGroupMessages {
+
             startsWith("开始监听") { name ->
                 val messagingServer = messagingServerList.find { it.groupId == group.id && it.name == name.trim() }
                 // 判断服务器是否没有添加
@@ -58,53 +60,20 @@ object Messaging : KotlinPlugin(
                     group.sendMessage("没有找到[${name.trim()}]服务器,请添加后在监听")
                     return@startsWith
                 }
-                // 判断 websocket 是否已经连接
-                if (messagingServer.status == ConnectStatus.CONNECTION) {
-                    group.sendMessage("[${name.trim()}]已经监听")
-                    return@startsWith
-                }
-                // 创建并添加 websocket
-                messagingServer.webSocketUtils = WebSocketUtils(messagingServer.url, object : WebSocketListener() {
-
-                    override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
-                        scope.launch { group.sendMessage("[${messagingServer.name}]服务器关闭:$code") }
-                        messagingServer.status = ConnectStatus.DISCONNECT
-                    }
-
-                    override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
-                        scope.launch {
-                            when(t){
-                                is ConnectException -> group.sendMessage("[${messagingServer.name}]连接失败,请检查服务器后重新监听")
-                                is EOFException -> group.sendMessage("[${messagingServer.name}]连接异常,请重新监听")
-                                else -> logger.error(t)
-                            }
-                        }
-                        messagingServer.status = ConnectStatus.DISCONNECT
-                    }
-
-                    override fun onMessage(webSocket: WebSocket, text: String) {
-                        scope.launch {
-                            runCatching {
-                                Json.decodeFromString<UserMessage>(text)
-                            }.onSuccess {
-                                group.sendMessage(PlainText("[${messagingServer.name}] <${it.name}> ").plus(it.toMiraiMessage()))
-                            }.onFailure {
-                                logger.error(it.localizedMessage)
-                            }
-                        }
-                    }
-
-                    override fun onOpen(webSocket: WebSocket, response: Response) {
-                        scope.launch { group.sendMessage("连接[${messagingServer.name}]服务器成功") }
-                        messagingServer.status = ConnectStatus.CONNECTION
-                    }
-                })
+                messagingServer.handler(group)
             }
 
-            always {msg ->
+            startsWith("全部监听") {
+                val messagingServerList = messagingServerList.filter { it.groupId == group.id}
+                messagingServerList.forEach {
+                    it.handler(group)
+                }
+            }
+
+            always { msg ->
                 val messagingServer = messagingServerList.find { it.groupId == group.id && msg.trim().startsWith(it.name) } ?: return@always
                 // 服务器未连接直接返回
-                if(messagingServer.status == ConnectStatus.DISCONNECT) {
+                if (messagingServer.status == ConnectStatus.DISCONNECT) {
                     group.sendMessage("[${messagingServer.name}]服务器未监听")
                     return@always
                 }
@@ -115,4 +84,60 @@ object Messaging : KotlinPlugin(
 
         }
     }
+
+    /**
+     * 处理转发服务器到群的消息
+     *
+     * @param group 群
+     */
+    private suspend fun MessagingServer.handler(group: Group){
+
+        // 判断是否已经连接
+        if (status == ConnectStatus.CONNECTION) {
+            group.sendMessage("[$name]已经监听")
+            return
+        }
+
+        webSocketUtils = WebSocketUtils(url, object : WebSocketListener() {
+
+            // 连接关闭
+            override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
+                launch { group.sendMessage("[$name]服务器关闭:$code") }
+                status = ConnectStatus.DISCONNECT
+            }
+
+            // 异常处理
+            override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
+                launch {
+                    when (t) {
+                        is ConnectException -> group.sendMessage("[$name]连接失败,请检查服务器后重新监听")
+                        is EOFException -> group.sendMessage("[$name]连接异常,请重新监听")
+                        is SocketTimeoutException -> group.sendMessage("[$name]连接超时,请检查服务器后重新监听")
+                        else -> logger.error(t)
+                    }
+                }
+                status = ConnectStatus.DISCONNECT
+            }
+
+            // 服务器发来消息
+            override fun onMessage(webSocket: WebSocket, text: String) {
+                launch {
+                    runCatching {
+                        Json.decodeFromString<UserMessage>(text)
+                    }.onSuccess {
+                        group.sendMessage(PlainText("[$name] <${it.name}> ").plus(it.toMiraiMessage()))
+                    }.onFailure {
+                        logger.error(it)
+                    }
+                }
+            }
+
+            // 连接服务器成功
+            override fun onOpen(webSocket: WebSocket, response: Response) {
+                launch { group.sendMessage("连接[$name]服务器成功") }
+                status = ConnectStatus.CONNECTION
+            }
+        })
+    }
+
 }
