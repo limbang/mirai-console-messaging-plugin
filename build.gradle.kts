2c8eb70d19ebed79f3094b77b8cb24d1fc828a32
limbang <495071565@qq.com>
2023-03-23 23:03:23 +0800

    build: 更新版本和版权设置

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 plugins {
-    val kotlinVersion = "1.7.20"
+    val kotlinVersion = "1.8.10"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
-    id("net.mamoe.mirai-console") version "2.12.3"
+    id("net.mamoe.mirai-console") version "2.14.0"
 }
 
 group = "top.limbang"
-version = "0.1.1"
+version = "0.1.2"
 
 
 repositories {
@@ -16,6 +16,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
     testImplementation(kotlin("test"))
 }
--- src/main/kotlin/Messaging.kt
+++ src/main/kotlin/Messaging.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 limbang and contributors.
+ * Copyright (c) 2022-2023 limbang and contributors.
  *
  * 此源代码的使用受 GNU AGPLv3 许可证的约束，该许可证可在"LICENSE"文件中找到。
  * Use of this source code is governed by the GNU AGPLv3 license that can be found in the "LICENSE" file.
@@ -38,7 +38,7 @@ object Messaging : KotlinPlugin(
     JvmPluginDescription(
         id = "top.limbang.messaging",
         name = "Messaging",
-        version = "0.1.0",
+        version = "0.1.2",
     ) {
         author("limbang")
     }
